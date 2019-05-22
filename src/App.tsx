c7b143c666879ec2e6a1126bd1b7348a353b9b6f
Mikhail Guskov <ringill@mail.ru>
2019-05-23 02:02:33 +0300

    prettier not workin on jsx as expended

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+interface IZ<Type> {
+  url: string;
+  z: number;
+}
+
+const z = {
+  a: "1",
+  b: "2"
+};
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <div>
+        <p>Greetings, traveler! Sign up today!</p>
+      </div>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -14,13 +31,12 @@ const App: React.FC = () => {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
